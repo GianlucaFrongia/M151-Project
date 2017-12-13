1fc34cf42d03c0616c7be0e8cf269171b88f0c00
eliareutlinger <elia.reutlinger@gibmit.ch>
2017-12-13 14:22:24 +0100

    removed unuseful directorys & updated gitignore
    removed unuseful directorys & updated gitignore

--- modul/start.php
+++ modul/start.php
@@ -1,3 +1,8 @@
+<?php
+
+          include("session.php");
+
+?>
 <div class="my-auto">
           <img src="img/logo1500.png" class="img-responsive img-background"/>
           <h1 class="mb-0">Max
