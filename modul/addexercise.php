0f0862f8ffb14a55ddb5546bc14b06114723596a
Valentino Rusconi <vaekek3@gmail.com>
2017-12-16 14:51:15 +0100

    addExercise
    implementiert

--- modul/addexercise.php
+++ modul/addexercise.php
@@ -1,1 +1,28 @@
-Add exercise 
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "m151_project";
+// Create connection
+//
+ $exercise = $_POST["exercise"];
+ echo $exercise;
+//
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$sql = "INSERT INTO tb_exercise (name) VALUES ('$exercise')";
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+
+?>
