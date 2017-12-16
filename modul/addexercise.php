c14a2da106f5a0ed328a5536a310190a076ea678
gianluca <gianluca.frongia@gibmit.ch>
2017-12-16 15:29:02 +0100

    added prepared statement

--- modul/addexercise.php
+++ modul/addexercise.php
@@ -1,28 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "m151_project";
-// Create connection
-//
+    define('__ROOT__', dirname(dirname(__FILE__)));
+    require_once(__ROOT__.'/database/database.php');
+
  $exercise = $_POST["exercise"];
- echo $exercise;
-//
-$conn = new mysqli($servername, $username, $password,$dbname);
 
-// Check connection
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
 
-$sql = "INSERT INTO tb_exercise (name) VALUES ('$exercise')";
-if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-mysqli_close($conn);
-
-?>
+$query = "INSERT INTO tb_exercise(name) VALUES (?)";
+$stmt = $mysqli->prepare($query);
+$stmt->bind_param("s", $exercise);
+$stmt->execute();
+$mysqli->close();
+echo "New record created successfully";
