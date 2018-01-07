8ffa998da126eb25c0178cb9b90d64d11ac80fd6
eliareutlinger <elia.reutlinger@gibmit.ch>
2018-01-07 06:39:13 +0100

    Some commends
    Some commends

--- database/database.php
+++ database/database.php
@@ -1,12 +1,15 @@
 <?php
 
+	//Variabeln definieren
 	$_db_host = "localhost";
 	$_db_database = "m151_project";
 	$_db_username = "fucuser";
 	$_db_passwort = "MdbwbSwtPiCa01:24U";
 
+	//Verbindung herstellen
     $mysqli = mysqli_connect($_db_host, $_db_username, $_db_passwort, $_db_database);
     
+	//Verbindung prüfen
     if (!$mysqli) {
         die('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
     }
