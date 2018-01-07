9d65b3d6d4cdfa82898885c523285f7e357363af
Valentino Rusconi <vaekek3@gmail.com>
2018-01-07 17:04:02 +0100

    Changed start

--- modul/start.php
+++ modul/start.php
@@ -3,65 +3,65 @@
 	//Session und Datenbankverbindung einbinden
 	include("session.php");
 	include("../database/database.php");
-    
+
 //-------------------------------------------------- | News Abschnitt generieren | ------------------------------------------------------
-	
+
 	//Variabeln initialisieren
     $news = "";
-    
+
 	//News aus der Datenbank lesen
 	$sql3 = "SELECT * FROM `tb_news` ORDER BY date DESC";
 	$result3 = $mysqli->query($sql3);
     if ($result3->num_rows > 0) {
 		//Jeden Eintrag an eine Variabel anhängen
 		while($row = $result3->fetch_assoc()) {
-         
+
             $newsEntry = '<b>'. $row['title'] .'</b><p>'. $row['content'] .'</p><br/>';
             $news = $news . utf8_encode($newsEntry);
-            
-        } 
+
+        }
     }
-    
+
 //-------------------------------------------------- | Userinformationen generieren | ------------------------------------------------------
-    
+
 	//Variabel initialisieren
     $count = 0;
-    
+
 	//User-Übungen zählen
 	$sql4 = "SELECT * FROM `tb_exercise` WHERE user_id = $userid;";
 	$result4 = $mysqli->query($sql4);
 	if ($result4->num_rows > 0) {
 		while($row = $result4->fetch_assoc()) {
             $count = $count + 1;
-        } 
+        }
     }
-	
+
 	//Variabel zuweisen, Variabel auf 0 setzen
     $cExercices = $count;
     $count = 0;
-    
+
 	//User-Pläne zählen
 	$sql5 = "SELECT * FROM `tb_plan` WHERE creator = $userid";
 	$result5 = $mysqli->query($sql5);
 	if ($result5->num_rows > 0) {
 		while($row = $result5->fetch_assoc()) {
             $count = $count + 1;
-        } 
+        }
     }
-	
+
 	//Variabel zuweisen
     $cPlans = $count;
-    
+
 	//Vor-/Nachname auslesen, "Registriert Seit" berechnen
 	$sql = "SELECT reg_date, firstname, lastname FROM tb_user WHERE username = '$username'";
 	$result = $mysqli->query($sql);
 	if ($result->num_rows > 0) {
 		while($row = $result->fetch_assoc()) {
-			
+
 			//Variabeln auslesen
 			$firstname = $row["firstname"];
 			$lastname = $row["lastname"];
-            
+
 			//"Registriert seit" berechnen
             $now = time();
             $treg_date = strtotime($row['reg_date']);
@@ -70,12 +70,12 @@
 
 		}
 	}
-	
+
 //-------------------------------------------------- | zufälligen Motivationsspruch generieren | ------------------------------------------------------
-	
+
 	//Variabel auf 0 setzen
 	$count = 0;
-	
+
 	//Alle Sprüche in der Datenbank zählen
 	$sql2 = "SELECT description FROM tb_motivations";
 	$result2 = $mysqli->query($sql2);
@@ -84,11 +84,11 @@
 			$count++;
 		}
 	}
-	
+
 	//Zufallszahl generieren und in neuem Query auslesen
 	$limit = rand(1, $count);
 	$sql3 = "SELECT title, description FROM tb_motivations LIMIT $limit";
-	
+
 	//Ausgelesenen Eintrag formatieren und in Variabel speichern
 	$result3 = $mysqli->query($sql3);
 	if ($result3->num_rows > 0) {
@@ -96,8 +96,8 @@
 			$quote = "<b>" . utf8_encode($row["title"]) . "</b><br/>" . utf8_encode($row["description"]);
 		}
 	}
- 
- 
+
+
 ?>
 <div class="col-lg-12">
 	<h1 class="mb-0"><?php echo $firstname; ?>
@@ -109,7 +109,9 @@
 	<p class="mb-5">
 		<?php echo $quote; ?>
 	</p>
-    <p>Herzlich willkommen auf dem Dashboard von FUC</p>
+    <p>Herzlich Willkommen auf dem Dashboard von FUC</p>
+		<p> Fitness Under Control ist deine Lösung den Papierkram endlich loszuwerden.
+			Wir bieten eine Lösung welche dir die Möglichkeit gibt deine Trainingspläne online zu verwalten. Du kannst eigene Übungen erfassen, eigene Pläne erstellen und Pläne anderer User verwenden. </p>
     <hr/>
     <h3>News vom Team</h3>
     <br/>
