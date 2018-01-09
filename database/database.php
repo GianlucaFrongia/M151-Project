6e87f7102dd3e9d2af4f9ee7815512f179dad078
eliareutlinger <elia.reutlinger@gibmit.ch>
2018-01-09 15:04:15 +0100

    Edited DB File
    Edited DB File

--- database/database.php
+++ database/database.php
@@ -2,10 +2,10 @@
 
     //CREATE USER 'fucuser'@'localhost' IDENTIFIED VIA mysql_native_password USING 'MdbwbSwtPiCa01:24U';GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'fucuser'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
 	//Variabeln definieren
-	$_db_host = process.env.db_host;
-	$_db_database = process.env.db_database;
-	$_db_username = process.env.db_username;
-	$_db_passwort = process.env.db_password;
+	$_db_host = "localhost";
+	$_db_database = "m151_project";
+	$_db_username = "fucuser";
+	$_db_passwort = "MdbwbSwtPiCa01:24U";
 
 	//Verbindung herstellen
     $mysqli = mysqli_connect($_db_host, $_db_username, $_db_passwort, $_db_database);
