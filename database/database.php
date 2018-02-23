ff5621972ee34a56acf399a59f79a5d80bb3efc2
eliareutlinger <elia.reutlinger@gibmit.ch>
2018-02-23 17:34:59 +0100

    DB Changes

--- database/database.php
+++ database/database.php
@@ -5,8 +5,23 @@
 	$_db_username = getenv('APP_DB_USER');
 	$_db_passwort = getenv('APP_DB_PASS');
 
+
+
 	//Verbindung herstellen
     $mysqli = mysqli_connect($_db_host, $_db_username, $_db_passwort, $_db_database);
+
+	/* check connection */
+	if (mysqli_connect_errno()) {
+	    printf("Connect failed: %s\n", mysqli_connect_error());
+	    exit();
+	}
+
+	/* return name of current default database */
+	if ($result = $mysqli->query("SELECT DATABASE()")) {
+	    $row = $result->fetch_row();
+	    printf("Default database is %s.\n", $row[0]);
+	    $result->close();
+	}
 
 	//Verbindung prüfen
     if (!$mysqli) {
