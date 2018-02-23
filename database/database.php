2dab0cd4d1571d071dea246b8860b1d5a68935e4
eliareutlinger <elia.reutlinger@gibmit.ch>
2018-02-23 17:11:33 +0100

    Updated database.php to work with Heroku

--- database/database.php
+++ database/database.php
@@ -1,11 +1,9 @@
 <?php
 
-    //CREATE USER 'fucuser'@'localhost' IDENTIFIED VIA mysql_native_password USING 'MdbwbSwtPiCa01:24U';GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'fucuser'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
-	//Variabeln definieren
-	$_db_host = "localhost";
-	$_db_database = "fitnessundercontrol";
-	$_db_username = "fucuser";
-	$_db_passwort = "MdbwbSwtPiCa01:24U";
+	$_db_host = getenv('APP_DB_HOST');
+	$_db_database = getenv('APP_DB_NAME');
+	$_db_username = getenv('APP_DB_USER');
+	$_db_passwort = getenv('APP_DB_PASS');
 
 	//Verbindung herstellen
     $mysqli = mysqli_connect($_db_host, $_db_username, $_db_passwort, $_db_database);
